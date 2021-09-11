554292e218804f3e50f82c96214644696d13fb72
Corey Mckeel <coreymckeel@Coreys-MacBook-Air.local>
2021-09-11 17:18:54 -0400

    Finished click event and alternating players

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 console.log("Testing the connection to javascript file");
-
+var player = "X"
+function chooseBox(id) {
+    console.log(id);
+    var element = document.getElementById(`${id}`)
+    if (element.innerHTML == "") {
+        document.getElementById(`${id}`).innerHTML = player;
+        if (player == "X") {
+            player = "O";
+        }
+        else {
+            player = "X"
+        }
+    }
+}
